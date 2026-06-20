c7c30b0a4b61be75ba5a5a5a79bc6b2504b92427
damowanglx <damowang123lx@163.com>
2026-06-21 03:28:28 +0800

    chore: switch server to new IP 43.138.0.88

--- pethelp-client/src/api/request.ts
+++ pethelp-client/src/api/request.ts
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://82.156.55.211:3000/api/v1';
-// TODO: switch to https://pethelp.cloud/api/v1 after domain real-name verification
+const BASE_URL = 'http://43.138.0.88:3000/api/v1';
+// TODO: switch to https://pethelp.cloud/api/v1 after ICP filing
 
 interface RequestOptions {
   url: string;
