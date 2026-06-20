847c6571c6a435fbcf896ca3ca5395b410ddd3f4
damowanglx <damowang123lx@163.com>
2026-06-21 03:37:38 +0800

    chore: use new IP 43.138.0.88 while ICP pending

--- pethelp-client/src/api/request.ts
+++ pethelp-client/src/api/request.ts
@@ -1,5 +1,5 @@
 const BASE_URL = 'http://43.138.0.88:3000/api/v1';
-// TODO: switch to https://pethelp.cloud/api/v1 after ICP filing
+// TODO: switch to https://pethelp.cloud/api/v1 after ICP filing approved
 
 interface RequestOptions {
   url: string;
